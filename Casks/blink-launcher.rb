14ce8e2a226766b42de966491513fb5875214387
Joseph Cerone <jpcerone@gmail.com>
2026-01-31 20:57:49 -0500

    Update SHA256 for v1.0.0

--- Casks/blink-launcher.rb
+++ Casks/blink-launcher.rb
@@ -1,6 +1,6 @@
 cask "blink-launcher" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256_AFTER_RELEASE"
+  sha256 "93c700293a615621a84bae048ff21ce95fa4f0bf543310df3c26d673f03a98e0"
 
   url "https://github.com/jpcerone/BlinkLauncher/releases/download/v#{version}/Blink.zip"
   name "Blink Launcher"
