eed305e98dd1fa66cd7d5bdbc5b94dfaf71713a7
Joseph Cerone <jpcerone@gmail.com>
2026-02-02 21:26:24 -0500

    fixing rb file

--- Casks/blink-launcher.rb
+++ Casks/blink-launcher.rb
@@ -1,6 +1,6 @@
 cask "blink-launcher" do
   version "1.0.1"
-  sha256:"ac30dbd0f91adc97eaf574d8f7485830685f5055b71650c154e1ea215e17a2b8"
+  sha256 "ac30dbd0f91adc97eaf574d8f7485830685f5055b71650c154e1ea215e17a2b8"
 
   url "https://github.com/jpcerone/BlinkLauncher/releases/download/v#{version}/Blink.zip"
   name "Blink Launcher"
